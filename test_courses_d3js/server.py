129c94a299866e2c547a2c6df16170bf25028350
rbsteinm <rbsteinm@gmail.com>
2017-10-03 17:47:03 +0200

    refactor indexing and relation retrieval

--- test_courses_d3js/server.py
+++ test_courses_d3js/server.py
@@ -35,20 +35,26 @@ def get_index():
     return app.send_static_file('index.html')
 
 
+# displays the graph when the page is loaded
 @app.route("/graph")
 def get_graph():
     db = get_db()
-    results = db.run("MATCH (c:Course) RETURN c.title as course")
+    results = db.run("MATCH (c:Course) RETURN ID(c) as node_id, c.code as code, c.title as course")
     nodes = []
     rels = []
+    index_dict = {} # contains matching between neo4j's node ids and node indexes for the front end
+    node_index = 0
     for record in results:
-        nodes.append({"title": record["course"], "label": "course"})
+        nodes.append({"node_id": record["node_id"], "code": record["code"], "title": record["course"], "label": "course", "index" : node_index})
+        assert(not record["node_id"] in index_dict)
+        index_dict[record["node_id"]] = node_index
+        node_index += 1
 
-    relationships = db.run("MATCH (c1:Course) -[r]->(c2:Course) RETURN c1.title as source, c2.title as target")
+    relationships = db.run("MATCH (c1:Course) -[r]->(c2:Course) RETURN ID(c1) as source, ID(c2) as target")
     for rel in relationships:
-        #print(rel["source"])
-        index_c1 = nodes.index({"title": rel["source"], "label": "course"})
-        index_c2 = nodes.index({"title": rel["target"], "label": "course"})
+
+        index_c1 = index_dict[rel["source"]]
+        index_c2 = index_dict[rel["target"]]
         rels.append({"source": index_c1, "target": index_c2})
 
     return Response(dumps({"nodes": nodes, "links": rels}),
