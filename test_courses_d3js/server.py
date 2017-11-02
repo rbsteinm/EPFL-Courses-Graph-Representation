6e03e09ed63a5a21156e68288306cb40631a3ec0
rbsteinm <rbsteinm@gmail.com>
2017-11-02 13:54:20 +0100

    directional edges server-side

--- test_courses_d3js/server.py
+++ test_courses_d3js/server.py
@@ -16,17 +16,28 @@ driver = GraphDatabase.driver('bolt://localhost', auth=basic_auth("neo4j", "pika
 # input: a node and its id
 # output: a dict containing the node's attributes
 def serialize_course(course, c_id):
-    return {
-        'title': course['title'],
-        'code': course['code'],
-        'resume': course['resume'],
-        'id': c_id
-}
+    return {'title': course['title'], 'code': course['code'], 'resume': course['resume'], 'id': c_id}
 
 def get_db():
     if not hasattr(g, 'neo4j_db'):
         g.neo4j_db = driver.session()
     return g.neo4j_db
+
+def get_edges_and_ajd_list(initial_list, db, edge_type, directional_edges=True):
+    adjacency_list = initial_list # neighboring information
+    rels = [] # all relations (edges)
+    relationships = db.run("MATCH (c1:Course)-[r:"+ edge_type + "]->(c2:Course) RETURN ID(c1) as source, ID(c2) as target, ID(r) as edge_id")
+    for rel in relationships:
+        index_source = rel['source']
+        index_target = rel['target']
+        edge_id = rel['edge_id']
+        rels.append({"source": index_source, "target": index_target, "edge_id": edge_id})
+        adjacency_list[index_source].add(edge_id)
+        if not directional_edges:
+            adjacency_list[index_target].add(edge_id)
+
+    adjacency_list = {k: list(v) for k, v in adjacency_list.items()} # remove duplicates
+    return [adjacency_list, rels]
 
 @app.teardown_appcontext
 def close_db(error):
@@ -44,14 +55,16 @@ def get_graph():
     results = db.run("MATCH (c:Course) RETURN ID(c) as node_id, c.code as code, c.title as course") # get all node records from the db in a list
 
     nodes = [] # all nodes
-    rels = [] # all relations (edges)
-    adjacency_list = {} # neighboring information
+    #rels = [] # all relations (edges)
+    empty_ajd_list = {} # neighboring information
     for record in results:
         nodes.append({"node_id": record["node_id"], "code": record["code"], "title": record["course"], "label": "course"})
-        adjacency_list[record["node_id"]] = set()
+        empty_ajd_list[record["node_id"]] = set()
 
-    relationships = db.run("MATCH (c1:Course) -[r]->(c2:Course) RETURN ID(c1) as source, ID(c2) as target, ID(r) as edge_id")
+    '''relationships = db.run("MATCH (c1:Course)-[r:REQUIRE_OBL]->(c2:Course) RETURN ID(c1) as source, ID(c2) as target, ID(r) as edge_id")
+    length = 0
     for rel in relationships:
+        length += 1
         index_c1 = rel['source']
         index_c2 = rel['target']
         edge_id = rel['edge_id']
@@ -60,10 +73,12 @@ def get_graph():
         #adjacency_list[index_c2].add(index_c1)
         adjacency_list[index_c1].add(edge_id)
         adjacency_list[index_c2].add(edge_id)
+    print(length)
 
     # remove duplicates
-    adjacency_list = {k: list(v) for k, v in adjacency_list.items()}
+    adjacency_list = {k: list(v) for k, v in adjacency_list.items()}'''
 
+    [adjacency_list, rels] = get_edges_and_ajd_list(empty_ajd_list, db, "REQUIRE_OBL", False)
 
     return Response(dumps({"nodes": nodes, "links": rels, "adjacency_list": adjacency_list}),
                     mimetype="application/json")
@@ -98,5 +113,5 @@ def get_neighbors():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True, host='192.168.1.111', port=8080)
+    #app.run(debug=False, host='128.179.152.254', port=8080)
     app.run(debug=True, port=8080)
