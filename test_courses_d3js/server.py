5819cf9ff46b22af5572f30666a3c73b77d3b271
rbsteinm <rbsteinm@gmail.com>
2017-11-07 14:48:22 +0100

    can now display different edge types (using dashed edges)

--- test_courses_d3js/server.py
+++ test_courses_d3js/server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from __future__ import print_function # In python 2.7
-import sys
+import sys, copy
 from json import dumps
 
 from flask import Flask, g, Response, request
@@ -23,21 +23,37 @@ def get_db():
         g.neo4j_db = driver.session()
     return g.neo4j_db
 
-def get_edges_and_ajd_list(initial_list, db, edge_type, directional_edges=True):
-    adjacency_list = initial_list # neighboring information
+# input: an empty ajdacency list and the db handle
+# output: a list of all the edges and a list of dictionnary, each one being an adjacency list (source noce -> edgeID) for a distinct edge type
+def get_edges_and_ajd_lists(initial_list, db, directional_edges=True):
+    adjacency_lists = dict() # will contain all the adjacency lists
+    adjacency_lists['ALL_EDGES'] = initial_list
+    for edge_type in get_edge_types(db):
+        adjacency_lists[edge_type] = copy.deepcopy(initial_list)
+    empty_ajd_list = initial_list # neighboring information
     rels = [] # all relations (edges)
-    relationships = db.run("MATCH (c1:Course)-[r:"+ edge_type + "]->(c2:Course) RETURN ID(c1) as source, ID(c2) as target, ID(r) as edge_id")
+    #relationships = db.run("MATCH (c1:Course)-[r:"+ edge_type + "]->(c2:Course) RETURN ID(c1) as source, ID(c2) as target, ID(r) as edge_id")
+    relationships = db.run("MATCH (c1:Course)-[r]->(c2:Course) RETURN ID(c1) as source, ID(c2) as target, ID(r) as edge_id, TYPE(r) as edge_type")
     for rel in relationships:
+        rel_type = rel['edge_type']
+        print(rel_type)
         index_source = rel['source']
         index_target = rel['target']
         edge_id = rel['edge_id']
-        rels.append({"source": index_source, "target": index_target, "edge_id": edge_id})
-        adjacency_list[index_source].add(edge_id)
-        if not directional_edges:
-            adjacency_list[index_target].add(edge_id)
+        rels.append({"source": index_source, "target": index_target, "edge_id": edge_id, "edge_type": rel_type})
+        adjacency_lists[rel_type][index_source].add(edge_id)
+        adjacency_lists['ALL_EDGES'][index_source].add(edge_id)
+        #if not directional_edges:
+        #    adjacency_list[index_target].add(edge_id)
 
-    adjacency_list = {k: list(v) for k, v in adjacency_list.items()} # turn set to list (set => no duplicates)
-    return [adjacency_list, rels]
+    for key, ajd_list in adjacency_lists.items():
+        adjacency_lists[key] = {k: list(v) for k, v in ajd_list.items()} # turn set to list (set => no duplicates)
+    return [adjacency_lists, rels]
+
+ # returns a list containing all distinct edge types as strings
+def get_edge_types(db):
+    results = db.run("MATCH (a)-[r]->(b) RETURN distinct(type(r)) as type")
+    return [record['type'] for record in results]
 
 @app.teardown_appcontext
 def close_db(error):
@@ -48,7 +64,7 @@ def close_db(error):
 def get_index():
     return app.send_static_file('index.html')
 
-# displays the graph when the page is loaded
+# sends a JSON to the front-end with all the nodes and the edgesets
 @app.route("/graph")
 def get_graph():
     db = get_db()
@@ -61,9 +77,9 @@ def get_graph():
         nodes.append({"node_id": record["node_id"], "code": record["code"], "title": record["course"], "label": "course"})
         empty_ajd_list[record["node_id"]] = set() # initialize the adjacency list with empty sets
 
-    [adjacency_list, rels] = get_edges_and_ajd_list(empty_ajd_list, db, "REQUIRE_OBL", False)
+    [adjacency_lists, rels] = get_edges_and_ajd_lists(empty_ajd_list, db)
 
-    return Response(dumps({"nodes": nodes, "links": rels, "adjacency_list": adjacency_list}),
+    return Response(dumps({"nodes": nodes, "links": rels, "adjacency_lists": adjacency_lists}),
                     mimetype="application/json")
 
 # return search bar results
@@ -92,7 +108,6 @@ def get_neighbors():
         db = get_db()
         neighbors = db.run("MATCH (n)-[*1..1]-(m) WHERE ID(n)="+nodeId+" RETURN distinct ID(m) as id")
         return Response(dumps([{'neigh':neigh['id']} for neigh in neighbors]), mimetype="application/json")
-
 
 
 if __name__ == '__main__':
